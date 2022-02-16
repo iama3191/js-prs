a09b9a360dc393290f960bb859455c17bcf30622
Irene <ire.andreina.z@gmail.com>
2022-02-16 15:07:30 +0100

    modify code to assure a valid input from the user

--- function.js
+++ function.js
@@ -1,25 +1,23 @@
 //alert('Let\'s play a game');
 
-//let playerSelection = prompt('Please pick your weapon: rock, paper, scissors').toLowerCase();
-
+let playerSelection = prompt('Please pick your weapon: rock, paper, scissors').toLowerCase();
 let option = ['rock','paper','scissors'];
-
+let message;
 let computerSelection = computerPlay();
-
-function computerPlay() {
-    return option[Math.floor(Math.random()*option.length)]
-}
-
-console.log(computerPlay());
 
 //This function will check the user's input
 //If it isn't rock, paper or scissors --> needs to enter a valid word
-function checkWord(playerSelection) {
-    if ((playerSelection==='rock')||(playerSelection==='paper')||(playerSelection==='scissors')) {
-        return playerSelection;
-    } else {
-        return alert('Word not valid');
-    }
-}
 
-//console.log(checkWord(playerSelection));
+    if (option.includes(playerSelection)) {
+        computerPlay();//in here I can assure that won't go on if there isn't a valid word
+        console.log(playerSelection);
+        console.log(computerPlay());
+    } else {
+        message='Word not valid';
+        console.log(message);
+    }
+
+//function that picks a random option for the computer
+function computerPlay() {
+    return option[Math.floor(Math.random()*option.length)]
+}
